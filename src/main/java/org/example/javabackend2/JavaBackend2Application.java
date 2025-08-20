e5b230cc2a87a5328efe42d2f200d6660501671e
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-08-20 14:12:19 +0200

    Initial commit

---
+++ src/main/java/org/example/javabackend2/JavaBackend2Application.java
@@ -0,0 +1,13 @@
+package org.example.javabackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javabackend2/JavaBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javabackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
