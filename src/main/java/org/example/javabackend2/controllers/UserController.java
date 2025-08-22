3bf76ec99acd94f430b742d80b178f1db452ef8e
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-08-22 11:31:15 +0200

    Added thymeleaf for login and products. Added login in UserController

--- src/main/java/org/example/javabackend2/controllers/UserController.java
+++ src/main/java/org/example/javabackend2/controllers/UserController.java
@@ -1,4 +1,20 @@
 package org.example.javabackend2.controllers;
 
+import org.example.javabackend2.models.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class UserController {
+
+    @GetMapping("/login")
+    public String showLogin(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(Model model, User user) {
+        return "products";
+    }
 }
