71e4d77fb33d0ed945dc69f39fe114cf5bf32065
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-08-22 12:14:07 +0200

    Bootstrapped some data to db

--- src/main/java/org/example/javabackend2/JavaBackend2Application.java
+++ src/main/java/org/example/javabackend2/JavaBackend2Application.java
@@ -1,7 +1,15 @@
 package org.example.javabackend2;
 
+import org.example.javabackend2.models.Role;
+import org.example.javabackend2.models.User;
+import org.example.javabackend2.repos.RoleRepository;
+import org.example.javabackend2.repos.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class JavaBackend2Application {
@@ -10,4 +18,16 @@ public class JavaBackend2Application {
         SpringApplication.run(JavaBackend2Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository, RoleRepository roleRepository) {
+        return args -> {
+
+            Role role = new Role();
+            role.setType("admin");
+            role = roleRepository.save(role);
+
+            userRepository.save(new User("Sigrun", "olafsdottir@4ever.se", "HemligtLösenord", role));
+
+        };
+    }
 }
