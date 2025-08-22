98fb9996f5f396c5c7203debdc58c9b89fa9dc7f
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-08-22 11:58:23 +0200

    Connected serviceinterface and serviceImpl

--- src/main/java/org/example/javabackend2/services/UserService.java
+++ src/main/java/org/example/javabackend2/services/UserService.java
@@ -1,5 +1,17 @@
 package org.example.javabackend2.services;
 
+import org.example.javabackend2.dtos.UserDetailedDto;
+import org.example.javabackend2.dtos.UserDto;
+import org.example.javabackend2.models.User;
+
 public interface UserService {
+
+    public UserDetailedDto userToUserDetailedDto(User user);
+
+    public User userDetailedDtoToUser(UserDetailedDto userDetailedDto);
+
+    public UserDto userToUserDto(User user);
+
+    public User userDtoToUser(UserDto userDto);
 
 }
--- src/main/java/org/example/javabackend2/services/impl/UserServiceImpl.java
+++ src/main/java/org/example/javabackend2/services/impl/UserServiceImpl.java
@@ -1,5 +1,36 @@
 package org.example.javabackend2.services.impl;
 
-public class UserServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.javabackend2.dtos.UserDetailedDto;
+import org.example.javabackend2.dtos.UserDto;
+import org.example.javabackend2.models.User;
+import org.example.javabackend2.repos.UserRepository;
+import org.example.javabackend2.services.UserService;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetailedDto userToUserDetailedDto(User user) {
+        return null;
+    }
+
+    @Override
+    public User userDetailedDtoToUser(UserDetailedDto userDetailedDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto userToUserDto(User user) {
+        return null;
+    }
+
+    @Override
+    public User userDtoToUser(UserDto userDto) {
+        return null;
+    }
 }
