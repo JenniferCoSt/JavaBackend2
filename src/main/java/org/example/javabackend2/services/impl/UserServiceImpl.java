fb5786dec60e2bb50f4bd939c0070568e5977ba6
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-08-26 22:21:57 +0200

    Implemented a mapperclass and restructured

--- src/main/java/org/example/javabackend2/services/impl/UserServiceImpl.java
+++ src/main/java/org/example/javabackend2/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.javabackend2.dtos.RoleDto;
 import org.example.javabackend2.dtos.UserDetailedDto;
 import org.example.javabackend2.dtos.UserDto;
+import org.example.javabackend2.mappers.UserMapper;
 import org.example.javabackend2.models.User;
 import org.example.javabackend2.repos.UserRepository;
 import org.example.javabackend2.services.UserService;
@@ -16,26 +17,7 @@ import java.util.ArrayList;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
-    @Override
-    public UserDetailedDto userToUserDetailedDto(User user) {
-        return UserDetailedDto.builder().id(user.getId()).name(user.getName()).email(user.getEmail()).password(user.getPassword()).role(new RoleDto(user.getRole().getId(), user.getRole().getType())).orders(new ArrayList<>()).build();
-    }
-
-    @Override
-    public User userDetailedDtoToUser(UserDetailedDto userDetailedDto) {
-        return null;
-    }
-
-    @Override
-    public UserDto userToUserDto(User user) {
-        return null;
-    }
-
-    @Override
-    public User userDtoToUser(UserDto userDto) {
-        return null;
-    }
+    private final UserMapper userMapper;
 
     @Override
     public UserDetailedDto findUserDetailedDtoByEmail(String email) {
@@ -43,6 +25,6 @@ public class UserServiceImpl implements UserService {
         if(user == null) {
             return null;
         }
-        return userToUserDetailedDto(user);
+        return userMapper.userToUserDetailedDto(user);
     }
 }
