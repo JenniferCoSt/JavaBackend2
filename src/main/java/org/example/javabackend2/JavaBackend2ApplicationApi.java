f500cb7c879dcbe2a460789d95cc3841f9760efb
JoakimSkappegard <joakim_skappegard@hotmail.com>
2025-08-28 10:28:55 +0200

    Bytt namn på aplikations startuppen för att förhindra merg konflikt (tänkt att vara två separata framöver pre docker)

--- src/main/java/org/example/javabackend2/JavaBackend2ApplicationApi.java
+++ src/main/java/org/example/javabackend2/JavaBackend2ApplicationApi.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaBackend2Application {
+public class JavaBackend2ApplicationApi {
 
     public static void main(String[] args) {
         System.out.println("hell");
-        SpringApplication.run(JavaBackend2Application.class, args);
+        SpringApplication.run(JavaBackend2ApplicationApi.class, args);
         System.out.println("klar");
     }
 
