af5b27805e7552120e692728917f531eb9b3a63a
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-09-03 14:43:50 +0200

    Merge register into master

--- src/main/java/org/example/javabackend2/webbservice/controllers/UserController.java
+++ src/main/java/org/example/javabackend2/webbservice/controllers/UserController.java
@@ -1,9 +1,14 @@
 package org.example.javabackend2.webbservice.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.example.javabackend2.webbservice.dtos.UserRegisterDto;
+import org.example.javabackend2.webbservice.models.User;
 import org.example.javabackend2.webbservice.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,6 +29,24 @@ public class UserController {
     @GetMapping("/home")
     public String showHomePage() {
         return "home";
+    }
+
+    @GetMapping("/register")
+    public String showRegister(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") UserRegisterDto userRegisterDto,
+                               Model model) {
+        boolean registrationSuccessful = userService.saveUser(userRegisterDto);
+        if (!registrationSuccessful) {
+            model.addAttribute("user", userRegisterDto);
+            model.addAttribute("error", "Choose a different email");
+            return "register";
+        }
+        return "redirect:/login";
     }
 
 }
--- src/main/java/org/example/javabackend2/webbservice/services/impl/UserServiceImpl.java
+++ src/main/java/org/example/javabackend2/webbservice/services/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.example.javabackend2.webbservice.models.User;
 import org.example.javabackend2.webbservice.repos.RoleRepository;
 import org.example.javabackend2.webbservice.repos.UserRepository;
 import org.example.javabackend2.webbservice.services.UserService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -21,6 +21,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetailedDto findUserDetailedDtoByEmail(String email) {
@@ -38,7 +39,6 @@ public class UserServiceImpl implements UserService {
             return false;
         }
         Role role = roleRepository.findByType(userRegisterDto.getRole()).orElse(null);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(userRegisterDto.getPassword());
         userRegisterDto.setPassword(encodedPassword);
 
