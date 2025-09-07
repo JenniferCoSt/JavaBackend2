8ca1ae184ccabbf547726cc89868b1dec3f5e749
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-09-07 19:34:52 +0200

    Added validation when for register user

--- src/main/java/org/example/javabackend2/webbservice/controllers/UserController.java
+++ src/main/java/org/example/javabackend2/webbservice/controllers/UserController.java
@@ -1,11 +1,13 @@
 package org.example.javabackend2.webbservice.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.javabackend2.webbservice.dtos.UserRegisterDto;
 import org.example.javabackend2.webbservice.models.User;
 import org.example.javabackend2.webbservice.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,13 +35,18 @@ public class UserController {
 
     @GetMapping("/register")
     public String showRegister(Model model) {
-        model.addAttribute("user", new User());
+        model.addAttribute("user", new UserRegisterDto());
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute("user") UserRegisterDto userRegisterDto,
+    public String registerUser(@Valid @ModelAttribute("user") UserRegisterDto userRegisterDto,
+                               BindingResult bindingResult,
                                Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
         boolean registrationSuccessful = userService.saveUser(userRegisterDto);
         if (!registrationSuccessful) {
             model.addAttribute("user", userRegisterDto);
--- src/main/java/org/example/javabackend2/webbservice/dtos/UserRegisterDto.java
+++ src/main/java/org/example/javabackend2/webbservice/dtos/UserRegisterDto.java
@@ -1,5 +1,8 @@
 package org.example.javabackend2.webbservice.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserRegisterDto {
+    @Size(message ="Name must be between 2 and 50 characters", min = 2, max = 50)
     private String name;
+    @Email(message = "Invalid email adress")
     private String email;
+    @Size(message = "Password must be between 2 and 50 characters", min = 2, max = 50)
     private String password;
+    @NotNull(message = "You have to choose a role")
     private String role;
 }
