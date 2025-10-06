f32e2adee9fd5b5da7e1c7ad151eb5b68dedd1d7
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-10-06 13:28:37 +0200

    Created testmethod saveProducts

--- ApiFetch/src/test/java/org/example/apifetch/service/ApiServiceTest.java
+++ ApiFetch/src/test/java/org/example/apifetch/service/ApiServiceTest.java
@@ -15,9 +15,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension .class)
 class ApiServiceTest {
@@ -26,38 +29,133 @@ class ApiServiceTest {
     ApiProductServiceImpl productService;
 
     @Mock
+    ApiCategoryService categoryService;
+
+    @Mock
     ApiProductRepo productRepo;
 
     @Mock
     ApiCategoryRepo categoryRepo;
 
-    private ProductApi productApi;
-    private CategoryApi categoryApi;
-    private RatingApi ratingApi;
+    private ProductApi productApi1;
+    private ProductApi productApi2;
+    private CategoryApi categoryApi1;
+    private CategoryApi categoryApi2;
+    private RatingApi ratingApi1;
+    private RatingApi ratingApi2;
 
-    private ProductDtoApi productDtoApi;
-    private CategoryDtoApi categoryDtoApi;
-    private RatingDtoApi ratingDtoApi;
+    private ProductDtoApi productDtoApi1;
+    private ProductDtoApi productDtoApi2;
+    private CategoryDtoApi categoryDtoApi1;
+    private CategoryDtoApi categoryDtoApi2;
+    private RatingDtoApi ratingDtoApi1;
+    private RatingDtoApi ratingDtoApi2;
+
+    List<ProductDtoApi> productDtos;
+    List<ProductApi> productApiList;
 
     @BeforeEach
     void setUp() {
-        CategoryDtoApi categoryDtoApi = new CategoryDtoApi();
+        categoryDtoApi1 = CategoryDtoApi.builder()
+                .id(1L)
+                .type("food")
+                .build();
 
+        categoryDtoApi2 = CategoryDtoApi.builder()
+                .id(2L)
+                .type("animal")
+                .build();
+
+        ratingDtoApi1 = RatingDtoApi.builder()
+                .rate(3.5)
+                .count(37)
+                .build();
+
+        ratingDtoApi2 = RatingDtoApi.builder()
+                .rate(4.8)
+                .count(29)
+                .build();
+
+        productDtoApi1 = ProductDtoApi.builder()
+                .id(11L)
+                .title("Apple")
+                .description("It's red")
+                .price(5.5)
+                .rating(ratingDtoApi1)
+                .category("food")
+                .image("apple.jpg")
+                .build();
+
+        productDtoApi2 = ProductDtoApi.builder()
+                .id(22L)
+                .title("Bird")
+                .description("It can fly")
+                .price(145.0)
+                .rating(ratingDtoApi2)
+                .category("animal")
+                .image("bird.jpg")
+                .build();
+
+        productDtos = List.of(productDtoApi1, productDtoApi2);
+
+        categoryApi1 = CategoryApi.builder()
+                .id(1L)
+                .type("food")
+                .build();
+
+        categoryApi2 = CategoryApi.builder()
+                .id(2L)
+                .type("animal")
+                .build();
+
+        ratingApi1 = RatingApi.builder()
+                .rate(3.5)
+                .count(37)
+                .build();
+
+        ratingApi2 = RatingApi.builder()
+                .rate(4.8)
+                .count(29)
+                .build();
+
+
+        productApi1 = ProductApi.builder()
+                .id(11L)
+                .title("Apple")
+                .description("It's red")
+                .price(5.5)
+                .rating(ratingApi1)
+                .category(categoryApi1)
+                .image("apple.jpg")
+                .build();
+
+        productApi2 = ProductApi.builder()
+                .id(22L)
+                .title("Bird")
+                .description("It can fly")
+                .price(145.0)
+                .rating(ratingApi2)
+                .category(categoryApi2)
+                .image("bird.jpg")
+                .build();
+
+        productApiList = List.of(productApi1, productApi2);
+    }
+
+    @Test
+    void ApiService_convertFromDto_returnProductApi(){
 
     }
 
     @Test
     void ApiService_saveProducts_success() {
+        when(categoryService.getCategoryFromTitel("food")).thenReturn(categoryApi1);
+        when(categoryService.getCategoryFromTitel("animal")).thenReturn(categoryApi2);
 
-        int productsBeforeLoad = productRepo.findAll().size();
-        int categoryBeforeLoad = categoryRepo.findAll().size();
+        productService.saveProducts(productDtos);
 
-        productService.fetchProductsFromApi();
+        verify(productRepo).saveAll(productApiList);
 
-        assertThat(productRepo.findAll()).isNotEmpty();
-        assertThat(categoryRepo.findAll()).isNotEmpty();
-
-        assertThat(productsBeforeLoad).isLessThan(productRepo.findAll().size());
-        assertThat(categoryBeforeLoad).isLessThan(categoryRepo.findAll().size());
     }
+
 }
