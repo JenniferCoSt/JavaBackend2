42106e23d53d6205a9886c2720f828e0a95b0a9e
Helenes-Hub <hrydros@gmail.com>
2025-10-07 15:16:17 +0200

    fixed contextload issue and added orderserviceTest fix

--- src/test/java/org/example/javabackend2/JavaBackend2ApplicationTests.java
+++ src/test/java/org/example/javabackend2/JavaBackend2ApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.javabackend2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = org.example.javabackend2.webbservice.JavaBackend2Application.class)
 class JavaBackend2ApplicationTests {
 
     @Test
