1b3c6196b9e9170bc3f620fd64da54037c2ff74d
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-10-07 15:02:36 +0200

    changed to h2 database

--- src/test/java/org/example/javabackend2/JavaBackend2ApplicationTests.java
+++ src/test/java/org/example/javabackend2/JavaBackend2ApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.javabackend2;
 
+import org.example.javabackend2.webbservice.JavaBackend2Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JavaBackend2Application.class)
 class JavaBackend2ApplicationTests {
 
     @Test
